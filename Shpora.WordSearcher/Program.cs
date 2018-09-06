ddb9bbeaa16943be35c84ffb3fee54b3410bc340
Leoltron <leoole5@gmail.com>
2018-09-07 00:54:58 +0500

    Added base

--- Shpora.WordSearcher/Program.cs
+++ Shpora.WordSearcher/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Shpora.WordSearcher
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
+            var (server, token) = ParseArgs(args);
+            var client = new HttpClient {BaseAddress = new Uri(server)};
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token", token);
+
+            var ws = new WordSearcher(client);
+            await ws.InitGameAsync(true);
+            await ws.UpdateStatsAsync();
+            await ws.FinishGameAsync();
+        }
+
+        private static (string server, string token) ParseArgs(string[] args)
+        {
+            if (args == null || args.Length != 2)
+            {
+                return ("http://shpora.skbkontur.ru:80/", "4b127b29-d38d-4c72-8276-e79e04c3095b");
+            }
+            return (args[0], args[1]);
         }
     }
 }
---
+++ Shpora.WordSearcher/WordSearcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Shpora.WordSearcher
+{
+    public class WordSearcher
+    {
+        private readonly HttpClient client;
+        public int Points { get; private set; }
+        public int Words { get; private set; }
+        public int Moves { get; private set; }
+
+        public WordSearcher(HttpClient client)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task InitGameAsync(bool test = false)
+        {
+            var request = "/task/game/start";
+            if (test)
+                request += "?test=true";
+
+            var response = await client.PostAsync(request, null);
+            response.EnsureSuccessStatusCode();
+            var secondsTimeout = long.Parse(response.Headers.GetValues("Expires").First());
+            var sessionInitDate = DateTime.Parse(string.Join(" ", response.Headers.GetValues("Last-Modified")));
+            Points = 0;
+        }
+
+        public async Task UpdateStatsAsync()
+        {
+            const string request = "/task/game/stats";
+            var response = await client.GetAsync(request);
+            var stats =
+                JsonConvert.DeserializeObject<Dictionary<string, int>>(await response.Content.ReadAsStringAsync());
+            Words = stats["words"];
+            Points = stats["points"];
+            Moves = stats["moves"];
+        }
+
+        public async Task FinishGameAsync()
+        {
+            const string request = "/task/game/finish";
+            var response = await client.PostAsync(request, null);
+            response.EnsureSuccessStatusCode();
+            Points =
+                JsonConvert.DeserializeObject<Dictionary<string, int>>(await response.Content.ReadAsStringAsync())[
+                    "points"];
+        }
+    }
+}
