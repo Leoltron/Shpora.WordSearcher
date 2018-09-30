400c7a6d36333a1ec7a0116293583be90d3242f1
Leoltron <leoole5@gmail.com>
2018-09-30 19:25:28 +0500

    Moved extension classes to it's own namespace

--- Shpora.WordSearcher/Extensions/ArrayExtensions.cs
+++ Shpora.WordSearcher/Extensions/ArrayExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Shpora.WordSearcher
+namespace Shpora.WordSearcher.Extensions
 {
     public static class ArrayExtensions
     {
@@ -55,25 +55,6 @@ namespace Shpora.WordSearcher
             }
 
             return hash;
-        }
-
-        public static bool ArrayEquals(this bool[,] array, bool[,] other)
-        {
-            if (array == null && other == null)
-                return true;
-            if (array == null || other == null)
-                return false;
-            if (array.GetLength(0) != other.GetLength(0) || array.GetLength(1) != other.GetLength(1))
-                return false;
-
-            for (var i = 0; i < array.GetLength(0); i++)
-            for (var j = 0; j < array.GetLength(1); j++)
-            {
-                if (array[i, j] != other[i, j])
-                    return false;
-            }
-
-            return true;
         }
     }
 }
